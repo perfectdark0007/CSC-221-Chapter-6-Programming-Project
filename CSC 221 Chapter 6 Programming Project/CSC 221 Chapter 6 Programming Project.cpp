028b8d1a1abcec0ddf3da53d4b86a0c11d20cbb5
perfectdark0007 <woodardwjun@gmail.com>
2025-05-06 12:16:27 -0400

    Add project files.

---
+++ CSC 221 Chapter 6 Programming Project/CSC 221 Chapter 6 Programming Project.cpp
@@ -0,0 +1,9 @@
+// CSC 221 Chapter 6 Programming Project.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello Professor Schaffner, hopefully this works!\n";
+}
